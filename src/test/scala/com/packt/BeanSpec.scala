1093018c7c92df252e922d6ef08068c08423d875
jake.barnes <jake.barnes@digital.hmrc.gov.uk>
2019-01-02 17:15:08 +0000

    Finished chapter 2

---
+++ src/test/scala/com/packt/BeanSpec.scala
@@ -0,0 +1,23 @@
+package com.packt
+
+import org.scalatest.FlatSpec
+
+class BeanSpec extends FlatSpec {
+
+  "Decimal" should "throw error when initialised with a non numeric string" in {
+    try {
+      Decimal("XYZ")
+    }
+    catch {
+      case e: IllegalArgumentException =>
+        assert(e.getMessage == "requirement failed: Unable to convert string to number")
+      case _ => fail
+    }
+  }
+  "Binary" should "throw error when initialised with a non numeric string" in {
+    intercept[IllegalArgumentException] {
+      Binary("XYZ")
+    }
+  }
+
+}
